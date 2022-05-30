ab199155ada2ef4e6138e0b7bc9e9d48b0ab9d06
Timofey <105107352+DIGIMONICH@users.noreply.github.com>
2022-05-31 02:54:58 +0300

    Добавлен вывод в консоль надписи при добавлении нового юзера в базу В мейн добавил стейтменты по заданию

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,26 +1,22 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        String sql = "CREATE TABLE IF NOT EXISTS user(id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                "name VARCHAR(50) NOT NULL," +
-                "lastName VARCHAR(50) NOT NULL," +
-                "age INT NOT NULL)";
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
-                .addAnnotatedClass(User.class)
-                .buildSessionFactory();
-        try {
-            Session session = factory.getCurrentSession();
-            session.beginTransaction();
-            session.createSQLQuery(sql).addEntity(User.class).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            factory.close();
-        }
+        UserService user = new UserServiceImpl();
+        user.createUsersTable();
+        user.saveUser("Timofey", "Nickishenko", (byte) 29);
+        user.saveUser("Petr", "Vasiliev", (byte) 17);
+        user.saveUser("Ivan", "Sidorov", (byte) 25);
+        user.saveUser("Daniil", "Pavlov", (byte) 31);
+        System.out.println(user.getAllUsers());
+        user.cleanUsersTable();
+        user.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -56,6 +56,7 @@ public class UserDaoHibernateImpl implements UserDao {
         } finally {
             factory.close();
         }
+        System.out.println("User с именем – \"" + name + "\" добавлен в базу данных");
     }
 
     @Override
